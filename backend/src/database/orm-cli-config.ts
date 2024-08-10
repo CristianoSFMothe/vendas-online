d66c623ce92276fa156427825198deef24738d86
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-08-09 23:52:33 -0300

    feat: insert data into the States and city table and create the State service and controller

--- backend/src/database/orm-cli-config.ts
+++ backend/src/database/orm-cli-config.ts
@@ -4,6 +4,8 @@ import { CreateTableUser1723255836106 } from 'src/migrations/1723255836106-Creat
 import { CreateTableAddress1723256813758 } from 'src/migrations/1723256813758-CreateTableAddress';
 import { CreateTableCity1723256808767 } from 'src/migrations/1723256808767-CreateTableCity';
 import { CreateTableState1723256818795 } from 'src/migrations/1723256818795-CreateTableState';
+import { InsertCities1723257734926 } from 'src/migrations/1723257734926-InsertCities';
+import { InsertStates1723257740033 } from 'src/migrations/1723257740033-InsertStates';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
@@ -13,6 +15,8 @@ export const dataSource = new DataSource({
     CreateTableAddress1723256813758,
     CreateTableCity1723256808767,
     CreateTableState1723256818795,
+    InsertCities1723257734926,
+    InsertStates1723257740033,
   ],
   logging: true,
 });
