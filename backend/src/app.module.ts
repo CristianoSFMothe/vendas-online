c75bf59789c969ed28726abaed319cd0788be36b
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-08-10 00:22:42 -0300

    feat: create cache manager

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,8 +4,16 @@ import { UserModule } from './models/user/user.module';
 import { CityModule } from './models/city/city.module';
 import { StateModule } from './models/state/state.module';
 import { AddressModule } from './models/address/address.module';
+import { CacheModule } from './cache/cache.module';
 @Module({
-  imports: [DatabaseModule, UserModule, CityModule, StateModule, AddressModule],
+  imports: [
+    DatabaseModule,
+    UserModule,
+    CityModule,
+    StateModule,
+    AddressModule,
+    CacheModule,
+  ],
   controllers: [],
   providers: [],
 })
