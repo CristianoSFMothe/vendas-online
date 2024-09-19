b1c6b474e3f39b44a8139c7e2e7eda024dc8b799
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-09-19 07:59:00 -0300

    feat: up corrections

--- backend/src/models/cart/cart.module.ts
+++ backend/src/models/cart/cart.module.ts
@@ -4,10 +4,16 @@ import { CartController } from './cart.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CartEntity } from './entities/cart.entity';
 import { CartProductModule } from '../cart-product/cart-product.module';
+import { ProductModule } from '../product/product.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CartEntity]), CartProductModule, ,],
+  imports: [
+    TypeOrmModule.forFeature([CartEntity]),
+    CartProductModule,
+    ProductModule,
+  ],
   providers: [CartService],
   controllers: [CartController],
+  exports: [CartService],
 })
 export class CartModule {}
